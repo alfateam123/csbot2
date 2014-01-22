3c8475e5952864de52790b142fc92930a5de8610
alfateam123 <alfateam123@gmail.com>
2014-01-23 00:28:12 +0100

    csbot2 can RSS

--- src/modules/rss.pm
+++ src/modules/rss.pm
@@ -5,15 +5,15 @@ use feature "say";
 
 #use JSON;
 use XML::FeedPP;
-#use List::Util qw(shuffle); #useful on catgirls, not here 
 #use LWP::Simple; #for tumblr 1280 image test
+use Data::Dumper;
 
 #my $hasLoaded=0;
 my @posts;
-#my @shownPosts;
+my @shownPosts;
 #now they are read from file.
-#my $sources=();
-#my @unloaded_sources=();
+my $sources=();
+my @unloaded_sources=();
 
 sub loadSource{
     my $source = shift;
@@ -23,12 +23,12 @@ sub loadSource{
 
     my $feed;
     eval{
-    	my $source_url=buildUrl($source);
+    	  my $source_url=$source; #buildUrl($source);
         $feed = XML::FeedPP->new($source_url);
     };
     if ($@)
     {
-        print "hey, failed! ".buildUrl($source);
+        print "hey, failed! ".$source;#buildUrl($source);
         #push(@unloaded_sources, $source);
         return; #next; 
     }
@@ -42,22 +42,41 @@ sub loadSource{
         #($cat_image_link, $isgoodpost) = extractFromReddit($post) if $source_type eq 'reddit';
         #($cat_image_link, $isgoodpost) = extractFromTumblr($post) if $source_type eq 'tumblr';
     
-        push (@posts, (:title => $post->title, :link => $post->link)) ;#if $isgoodpost; #@cat);
+        my $title = $post->title;
+        $title =~ s/(\t|\n)/ /g; #spaces are important, nowadays
+        $title =~ s/ {2,}/ /g; #here too...
+
+        push (@posts, {title => $title, link => $post->link}) ;#if $isgoodpost; #@cat);
     }
 
 }
 
 sub parse
 {
-    my ($self, $line, $irc, $config, $channel, $nick) = @_;
-    if (/^:(.+?)!.+?@.+? PRIVMSG ${channel} :rss (.+?)$/i)
+  my ($self, $line, $irc, $config, $channel, $nick) = @_;
+  if(/^:(.+?)!.+?@.+? PRIVMSG ${channel} :rss help/i)
+  {
+    say $irc "PRIVMSG $channel :(help) rss [number of item to see] <url>";
+  }
+  elsif (/^:(.+?)!.+?@.+? PRIVMSG ${channel} :rss ([0-9]* ?)(.+?)$/i)
+  {
+    loadSource($3);
+    unless (scalar  @posts)
     {
-        loadSources($1);
-        foreach my $post (@posts)
-        {
-        	say $irc "PRIVMSG $channel :".$post->{"title"}." --> ".$post->{"link"};
-        }
-        #say $irc "PRIVMSG $channel :output.";
+      say $irc "PRIVMSG $channel :I heard you talkin' shit, man. Please, stop it.";
     }
+    else
+    {
+      my ($index,$limit) = (0, 3);
+      $limit = int($2) if $2;
+      foreach my $post (@posts)
+      {
+        #say Dumper($post);
+    	  say $irc "PRIVMSG $channel :".$post->{"title"}." --> ".$post->{"link"} if $index < $limit;
+        $index++;
+      }
+    }
+    #say $irc "PRIVMSG $channel :output.";
+  }
 }    
 1;
